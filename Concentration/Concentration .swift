4571dfbdbfdb061e7a6c7c8d8d775ec64218e7fa
Emelyanov Roman <wohagy@gmail.com>
2021-12-01 21:33:59 +0300

    remade indexOfOneAndOnlyFaceUpCard with using clousers

--- Concentration/Concentration .swift
+++ Concentration/Concentration .swift
@@ -22,17 +22,18 @@ struct Concentration {
     
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter{cards[$0].isFaceUp}.oneAndOnly
+//            var foundIndex: Int?
+//            for index in cards.indices {
+//                if cards[index].isFaceUp {
+//                    if foundIndex == nil {
+//                        foundIndex = index
+//                    } else {
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
         }
         set {
             for index in cards.indices {
@@ -107,5 +108,11 @@ struct Concentration {
 extension Date {
     var sinceNow: Int {
         return -Int(self.timeIntervalSinceNow)
+    }
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
